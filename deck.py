e03f54da7c7c92b78e58c5bac6406d71c7d4742b
kottmans <40611149+kottmans@users.noreply.github.com>
2018-06-26 17:22:22 -0500

    Add files via upload

---
+++ deck.py
@@ -0,0 +1,22 @@
+from card import *
+
+class Deck:
+    def __init__(self):
+        suits = ("Spades", "Hearts", "Clubs", "Diamonds")
+        cardList = []
+        for suit in suits:
+            for rank in range(1,14):
+                cardList.append(Card(rank, suit))
+
+        self.cardList = cardList
+
+    def shuffleDeck(self):
+        for i in range(0, 5): # shuffle multiple times to improve randomness
+            shuffle(self.cardList)
+
+    def draw(self):
+        return self.cardList.pop()
+
+    def printDeck(self):
+        for card in self.cardList:
+            print(str(card))
