2612253a86b460570f0741add8ed81cc0ee2bcd5
kottmans <40611149+kottmans@users.noreply.github.com>
2018-06-26 16:23:58 -0500

    Add Card Class

---
+++ card.py
@@ -0,0 +1,30 @@
+class Card:
+    def __init__(self, rank, suit):
+        
+        # check to see if card is legal
+        suits = ("Spades", "Hearts", "Clubs", "Diamonds")
+        if rank not in range(1, 14):
+            raise TypeError("Rank must be an integer value between 1 and 13")
+        if suit not in suits:
+            raise TypeError("Suit must be found in suits")
+
+        self.rank = rank
+        self.suit = suit
+
+    """
+    Returns a string containing the name of the card
+    """
+    def cardName(self):
+        if self.rank == 1:
+            rankName = "Ace"
+        elif self.rank == 11:
+            rankName = "Jack"
+        elif self.rank == 12:
+            rankName = "Queen"
+        elif self.rank == 13:
+            rankName = "King"
+        else:
+            rankName = str(self.rank)
+
+        return "{rank} of {suit}".format(rank = rankName, suit = self.suit)
+    
